df999c593993010bf1ff8816f34c8e649bd24a67
Computhink-CS147 <cs147@computhink.com.sg>
2026-09-06 03:50:02 +0000

    Sep 6, 2026, 3:50 AM

--- testquestion2.js
+++ testquestion2.js
@@ -3,18 +3,5 @@ let ypos=50;
 let colorme=0;
 function setup(){
     createCanvas(600,600);
-    background("gray");
-
-   
-    
-    for(let num=1; num <1; num=num+1){
-        circle(xpos,ypos,50);
-
-            xpos+=50;
-            ypos+=50;
-            colorme+=30;
-    };
-
-    
-
+    background("gray")
 }
